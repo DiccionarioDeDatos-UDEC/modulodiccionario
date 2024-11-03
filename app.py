d1426ac76e19b0bcfc0c87fb0bc153ad0a7ff9be
JorgeDDuran <jorgerena3@gmail.com>
2024-11-03 08:46:13 -0500

    Diccionario de datos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS  # Importa CORS
 from modulo_diccionario.views import modulo_diccionario
-from modulo_ddl_dml.views import modulo_ddl_dml
+
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ CORS(app)  # Permite solicitudes desde cualquier origen
 
 # Registra los Blueprints
 app.register_blueprint(modulo_diccionario)
-app.register_blueprint(modulo_ddl_dml)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

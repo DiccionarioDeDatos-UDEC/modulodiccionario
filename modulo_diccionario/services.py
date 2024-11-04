0e2b24c69130dc18bf5790d48d0463e3586fab0c
Afalonso-A2 <117043182+Afalonso-A2@users.noreply.github.com>
2024-11-03 21:35:01 -0500

    services.py

--- modulo_diccionario/services.py
+++ modulo_diccionario/services.py
@@ -1,35 +1,33 @@
-#modulo_diccionario/servicios.py
-import json
+import json #Usar datos en JSon 
 from .models import cargar_datos_json, guardar_datos_json, agregar_tabla, obtener_tablas, obtener_columnas_de_tabla, agregar_relacion, obtener_relaciones, eliminar_relacion 
 
-# Servicio para agregar una tabla con columnas
+#Agregar tabla
 def servicio_agregar_tabla(nombre, descripcion, columnas):
     if verificar_nombre_tabla_existente(nombre):
-        return False  # Nombre ya existe, no se puede agregar
-    agregar_tabla(nombre, descripcion, columnas)
+        return False  
+        #Si ya esxiste no deja 
+    agregar_tabla(nombre, descripcion, columnas) #AlonDur
     return True  # Se agregó con éxito
 
 def verificar_nombre_tabla_existente(nombre):
-    tablas = obtener_tablas()  # Esta función debe devolver todas las tablas
+    tablas = obtener_tablas()  # Esta función debe devolver todas las tablas para ver si hay una con ese nombre
     return any(tabla['nombre'] == nombre for tabla in tablas)  # Compara el nombre
 
-# Servicio para obtener todas las tablas
-def servicio_obtener_tablas():
+
+def servicio_obtener_tablas():    #Retorna las tablas
     return obtener_tablas()
 
-def servicio_obtener_columnas(nombre_tabla):
+def servicio_obtener_columnas(nombre_tabla):    #Retorna las columnas
     return obtener_columnas_de_tabla(nombre_tabla)
 
-def crear_relacion(tabla_origen, columna_origen, tabla_destino, columna_destino, tipo_relacion):
-    """Crea una nueva relación entre tablas."""
+def crear_relacion(tabla_origen, columna_origen, tabla_destino, columna_destino, tipo_relacion):     #Crea relacion 
+   #AlonDur
     agregar_relacion(tabla_origen, columna_origen, tabla_destino, columna_destino, tipo_relacion)
 
 def listar_relaciones():
-    """Obtiene todas las relaciones entre tablas."""
     return obtener_relaciones()
 
 def borrar_relacion(tabla_origen, tabla_destino):
-    """Elimina una relación entre tablas."""
     eliminar_relacion(tabla_origen, tabla_destino)
 
 
